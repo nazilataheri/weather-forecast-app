703eceb91fa5ac3f687114939763238db6648f8c
nazilataheri <50323320+nazilataheri@users.noreply.github.com>
2021-02-24 19:58:10 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a react redux app that takes city name as an input and shows 5 day forcast data from site https://openweathermap.org/ with an API call.
+
+### Getting Started
+
+```
+> git clone git@github.com:nazilataheri/react-redux-weather.git
+> cd react-redux-weather
+> npm install
+> npm start
+```
---
+++ src/components/chart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine,
+} from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+  return _.round(_.sum(data) / data.length);
+}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => {
+  return (
+    <div>
+      <Sparklines height={100} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type='avg' />
+      </Sparklines>
+      <div>
+        {average(props.data)} {props.units}
+      </div>
+    </div>
+  );
+};
